9d8be36f8b69b487e2ffb3d22b88e5191a125ac0
Miles Oldenburger <32121785+60MilesPerHour@users.noreply.github.com>
2023-02-22 16:50:51 -0800

    Update Hover-DNS.py

--- Hover-DNS.py
+++ Hover-DNS.py
@@ -7,7 +7,7 @@ import requests
 options = webdriver.ChromeOptions()
 
 # Set up the ChromeDriver executable path
-service = ChromeService(executable_path='/home/miles/Webscraping/chromedriver')
+service = ChromeService(executable_path='/CHROMEDRIVER/PATH/GOES/HERE')
 
 # Set up for headless execution
 options.add_argument('--headless')
@@ -25,18 +25,18 @@ browser.get('https://www.hover.com/signin')
 # Find the username field and enter the username
 print("Logging in to hover.com...")
 time.sleep(5)
-username = browser.find_elements('xpath', '//*[@id="app"]/div/form/div/div[1]/div/label/div[2]/div[1]/input')
+username = browser.find_elements('xpath', 'YOUR_XPATHS')
 username[0].send_keys('USERNAME')
 
 # Find the password field and enter the password
-password = browser.find_elements('xpath', '//*[@id="app"]/div/form/div/div[2]/div/label/div[2]/div[1]/input')
+password = browser.find_elements('xpath', 'YOUR_XPATHS')
 password[0].send_keys('PASSWORD')
 
 # Wait for 5 seconds
 time.sleep(5)
 
 # Find the login button and click it
-browser.find_element('xpath', '//*[@id="app"]/div/form/div/div[3]/div[2]/button').click()
+browser.find_element('xpath', 'YOUR_XPATHS').click()
 
 # Wait for 5 seconds
 time.sleep(5)
@@ -45,31 +45,33 @@ time.sleep(5)
 print("Login successful! Updating DNS records...")
 
 # Find the domain name and click it
-browser.find_element('xpath', '//*[@id="app"]/div/div/div[2]/div/div[2]/div/div/div/div[3]/div[2]/a').click()
+browser.find_element('xpath', 'YOUR_XPATHS').click()
 
 # Wait for 5 seconds
 time.sleep(5)
 
 # Find the DNS records and update them
-browser.find_element('xpath', '//*[@id="app"]/div/div/div[2]/div/div[1]/div[2]/div[2]/ul/li[2]/a').click()
+browser.find_element('xpath', 'YOUR_XPATHS').click()
 time.sleep(2)
 
 # Update the * DNS record
-browser.find_element('xpath', '//*[@id="app"]/div/div/div[2]/div/div[2]/div/div[2]/div/div/div/div[2]/div[7]/div').click()
+browser.find_element('xpath', 'YOUR_XPATHS').click()
 time.sleep(2)
-browser.find_element('xpath', '/html/body/div[4]/div/div/div/form/div/div[1]/div[2]/div[3]/label/div[2]/div[1]/input').clear()
-browser.find_element('xpath', '/html/body/div[4]/div/div/div/form/div/div[1]/div[2]/div[3]/label/div[2]/div[1]/input').send_keys(ip)
+browser.find_element('xpath', 'YOUR_XPATHS').clear()
+browser.find_element('xpath', 'YOUR_XPATHS').send_keys(ip)
 print("Your * DNS record has been updated to: " + ip)
-browser.find_element('xpath', '/html/body/div[4]/div/div/div/form/div/div[2]/button[2]').click()
+# click the save button
+browser.find_element('xpath', 'YOUR_XPATHS').click()
 
 # Update the @ DNS record
-browser.find_element('xpath', '//*[@id="app"]/div/div/div[2]/div/div[2]/div/div[2]/div/div/div/div[3]/div[7]/div').click()
+browser.find_element('xpath', 'YOUR_XPATHS').click()
 time.sleep(2)
-browser.find_element('xpath', '/html/body/div[4]/div/div/div/form/div/div[1]/div[2]/div[3]/label/div[2]/div[1]/input').clear()
+browser.find_element('xpath', 'YOUR_XPATHS').clear()
 # Update the @ DNS record
-browser.find_element('xpath', '/html/body/div[4]/div/div/div/form/div/div[1]/div[2]/div[3]/label/div[2]/div[1]/input').send_keys(ip)
+browser.find_element('xpath', 'YOUR_XPATHS').send_keys(ip)
 print("Your @ DNS record has been updated to: " + ip)
-browser.find_element('xpath', '/html/body/div[4]/div/div/div/form/div/div[2]/button[2]').click()
+# click the save button
+browser.find_element('xpath', 'YOUR_XPATHS').click()
 
 # Wait for 5 seconds
 time.sleep(5)
